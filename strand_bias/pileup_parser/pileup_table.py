e33b38c5ce4111c622e293888bc7961349210e6f
TDMedina <tylerdanmedina@gmail.com>
2024-04-19 17:44:45 +0100

    Updated table tools.

--- strand_bias/pileup_parser/pileup_table.py
+++ strand_bias/pileup_parser/pileup_table.py
@@ -9,65 +9,65 @@ import plotly.io as pio
 pio.renderers.default = "browser"
 
 
-@register_dataframe_accessor("pileup_tools")
-class PileupTable:
-    def __init__(self, pandas_obj):
-        self._obj = pandas_obj
+# @register_dataframe_accessor("pileup_tools")
+# class PileupTable:
+#     def __init__(self, pandas_obj):
+#         self._obj = pandas_obj
+#
+#     @staticmethod
+#     def read_csv(file_path):
+#         table = pd.read_csv(file_path, sep="\t", index_col=[0, 1, 2], header=[0, 1])
+#         return table
+#
+#     def make_asymmetry_summary_table(self, by="coding_strand",
+#                                      as_proportion=False, as_ratio=False):
+#         summary_table = (self._obj
+#                          .groupby(["reference", by]).agg(sum)
+#                          .groupby(axis=1, level="alt").agg(sum))
+#         if as_proportion:
+#             summary_table = summary_table.groupby("reference").agg(self._make_summary_proportion)
+#         elif as_ratio:
+#             summary_table = summary_table.groupby("reference").agg(self._make_summary_ratio)
+#         summary_table = summary_table.loc[["C", "G"], ["A", "T"]]
+#         return summary_table
+#
+#     def calculate_orientation_bias_by_coding_strand(self, drop_non_ox=True):
+#         results = self._obj.groupby("alt", axis=1).agg(sum)
+#         results = results.loc[idx[:, :, "F1R2"]] / results.loc[idx[:, :, "F2R1"]]
+#         if drop_non_ox:
+#             results = results.loc[idx[["C", "G"], :], ["A", "T"]]
+#         return results
+#
+#     def calculate_strand_bias_by_coding_strand(self, drop_non_ox=True):
+#         results = self._obj.groupby(["reference", "coding_strand"]).agg(sum)
+#         results = results.groupby("alt", axis=1).agg(self._agg_div_alignment)
+#         if drop_non_ox:
+#             results = results.loc[idx[["C", "G"], :], ["A", "T"]]
+#         return results
+#
+#     @staticmethod
+#     def _agg_div_alignment(df):
+#         df = df.droplevel("alt", axis=1)
+#         return df.forward / df.reverse
+#
+#     @staticmethod
+#     def _make_summary_proportion(pair):
+#         if not len(pair) > 1 or min(pair) == 0:
+#             return pd.NA
+#         norm = min(pair)
+#         pair = ["1" if val == norm else f"{val/norm:.2f}" for val in pair]
+#         pair = f"{pair[0]}:{pair[1]}"
+#         return pair
+#
+#     @staticmethod
+#     def _make_summary_ratio(pair):
+#         if not len(pair) > 1 or min(pair) == 0:
+#             return pd.NA
+#         pair = pair[0] / pair[1]
+#         return pair
 
-    @staticmethod
-    def read_csv(file_path):
-        table = pd.read_csv(file_path, sep="\t", index_col=[0, 1, 2], header=[0, 1])
-        return table
 
-    def make_asymmetry_summary_table(self, by="coding_strand",
-                                     as_proportion=False, as_ratio=False):
-        summary_table = (self._obj
-                         .groupby(["reference", by]).agg(sum)
-                         .groupby(axis=1, level="alt").agg(sum))
-        if as_proportion:
-            summary_table = summary_table.groupby("reference").agg(self._make_summary_proportion)
-        elif as_ratio:
-            summary_table = summary_table.groupby("reference").agg(self._make_summary_ratio)
-        summary_table = summary_table.loc[["C", "G"], ["A", "T"]]
-        return summary_table
-
-    def calculate_orientation_bias_by_coding_strand(self, drop_non_ox=True):
-        results = self._obj.groupby("alt", axis=1).agg(sum)
-        results = results.loc[idx[:, :, "F1R2"]] / results.loc[idx[:, :, "F2R1"]]
-        if drop_non_ox:
-            results = results.loc[idx[["C", "G"], :], ["A", "T"]]
-        return results
-
-    def calculate_strand_bias_by_coding_strand(self, drop_non_ox=True):
-        results = self._obj.groupby(["reference", "coding_strand"]).agg(sum)
-        results = results.groupby("alt", axis=1).agg(self._agg_div_alignment)
-        if drop_non_ox:
-            results = results.loc[idx[["C", "G"], :], ["A", "T"]]
-        return results
-
-    @staticmethod
-    def _agg_div_alignment(df):
-        df = df.droplevel("alt", axis=1)
-        return df.forward / df.reverse
-
-    @staticmethod
-    def _make_summary_proportion(pair):
-        if not len(pair) > 1 or min(pair) == 0:
-            return pd.NA
-        norm = min(pair)
-        pair = ["1" if val == norm else f"{val/norm:.2f}" for val in pair]
-        pair = f"{pair[0]}:{pair[1]}"
-        return pair
-
-    @staticmethod
-    def _make_summary_ratio(pair):
-        if not len(pair) > 1 or min(pair) == 0:
-            return pd.NA
-        pair = pair[0] / pair[1]
-        return pair
-
-
-@register_dataframe_accessor("concat_pileup_tools")
+@register_dataframe_accessor("mismatch_tools")
 class ConcatenatedPileupTable:
     def __init__(self, pandas_obj):
         self._obj = pandas_obj
@@ -79,6 +79,7 @@ class ConcatenatedPileupTable:
 
     def simplify_for_reference_asymmetry(self, change_numerator, change_denominator,
                                          add_ratio_column=True, log_transform_ratio=False,
+                                         add_fraction_column=True,
                                          normalization_counts=None, normalization_factor=1):
         table = (self._obj
                  .groupby(["project_id", "case_id", "file_id", "reference"])
@@ -95,12 +96,15 @@ class ConcatenatedPileupTable:
 
         if add_ratio_column:
             ratio_label = f"{change_numerator}{change_denominator}_ratio".lower()
-            ratios = (table[change_numerator]
-                      / (table[change_denominator]+table[change_numerator]))
+            ratios = table[change_numerator] / table[change_denominator]
             if log_transform_ratio:
-                table[ratio_label] = log2(ratios)
-            else:
-                table[ratio_label] = ratios
+                ratios = log2(ratios)
+            table[ratio_label] = ratios
+        if add_fraction_column:
+            ratio_label = f"{change_numerator}{change_denominator}_fraction".lower()
+            ratios = (table[change_numerator]
+                      / (table[change_denominator] + table[change_numerator]))
+            table[ratio_label] = ratios
         return table
 
     def subset_oxo_nucleotides(self):
