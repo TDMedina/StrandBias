c5985e479b766009524ea35b1bacff276fc7b665
TDMedina <tylerdanmedina@gmail.com>
2024-06-05 21:32:03 +0100

    Fixed cleanup name.

--- wgs_pipeline/analysis_pipeline.sh
+++ wgs_pipeline/analysis_pipeline.sh
@@ -135,5 +135,5 @@ dest_dir="$(dirname "${input_bam}")"
 file_id="$(basename "${input_bam}")"
 file_id="${file_id%%.bam}"
 
-bash 05.cleanup.sh -d "${dest_dir}" -f "${file_id}"
+bash cleanup.sh -d "${dest_dir}" -f "${file_id}"
 rm "${input_bam}" "${input_bam%.bam}".bai
